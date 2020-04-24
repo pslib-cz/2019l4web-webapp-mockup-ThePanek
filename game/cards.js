0ffc89e60ec2c0f39dfb83fe5b702916bc70821a
ThePanek <60350052+ThePanek@users.noreply.github.com>
2020-04-24 08:46:00 +0200

    Add files via upload

---
+++ game/cards.js
@@ -0,0 +1,539 @@
+[{
+        type: "black",
+        text: "Proč v noci nemůžu spát? "
+    },
+    {
+        type: "black",
+        text: "Kdo je podivný? "
+    }, {
+        type: "black",
+        text: "Co to tak smrdí? "
+    },
+    {
+        type: "black",
+        text: "Jak jsem přišla o panenství? "
+    }, {
+        type: "black",
+        text: "Erekce trvající přes několik hodin. "
+    },
+    {
+        type: "black",
+        text: "Mám 99 problémů, ale ___ mezi nima není. "
+    }, {
+        type: "black",
+        text: "Jakou hračku dají do cereálií příště? "
+    },
+    {
+        type: "black",
+        text: "Paci, paci, pacičky, máma koupí botičky. A tatínek ___, za myší ocásek. "
+    }, {
+        type: "black",
+        text: "Je smutné, kolik dětí se dnes zaplétá do ___. "
+    },
+    {
+        type: "black",
+        text: "Alternativní medicína nově objevuje léčivý potenciál ___. "
+    }, {
+        type: "black",
+        text: "Co je to za zvuk? "
+    },
+    {
+        type: "black",
+        text: "Kvůli čemu skončil můj poslední vztah? "
+    }, {
+        type: "black",
+        text: "V nové reality show na MTV vystupuje osm zašlých celebrit, které žijí s ___. "
+    },
+    {
+        type: "black",
+        text: "Piju, abych zapomněl na ___. "
+    }, {
+        type: "black",
+        text: "Promiňte, pane profesore, ale ten úkol jsem nemohl dokončit kvůli ___. "
+    },
+    {
+        type: "black",
+        text: "Jaká je Batmanova tajná neřest? "
+    }, {
+        type: "black",
+        text: "Omlouváme se cestujícím za zpoždění vlaku. Vlak je opožděn z důvodu ___ na trati. "
+    },
+    {
+        type: "black",
+        text: "Co je dívčiným nejlepším přítelem? "
+    }, {
+        type: "black",
+        text: "Podle pravidel TSA je nyní v letadle zakázáno ___. "
+    },
+    {
+        type: "black",
+        text: "V novém filmu od Disney Channel se Hannah Montana poprvé potýká s ___. "
+    }, {
+        type: "black",
+        text: "Blaničtí rytíři vyjedou, až na nás zaútočí ___. "
+    },
+    {
+        type: "black",
+        text: "Milá Abinko, dělá mi potíže ___ a byla bych Ti vděčná za radu. "
+    }, {
+        type: "black",
+        text: "Místo brambor letos Ježíšek zlobivým dětem nadělí ___. "
+    },
+    {
+        type: "black",
+        text: "Co je nejvíc emo? "
+    }, {
+        type: "black",
+        text: "Žádné romantické večeři u svíček nesmí chybět ___. "
+    },
+    {
+        type: "black",
+        text: "___. O co že ti jedno stačit nebude! "
+    }, {
+        type: "black",
+        text: "Běloši mají rádi ___. "
+    },
+    {
+        type: "black",
+        text: "Další kniha J. K. Rowlingové: Harry Potter a komnata ___. "
+    }, {
+        type: "black",
+        text: "Ahoj, já jsem Horst Fuchs a chtěl bych vám představit ___. "
+    },
+    {
+        type: "black",
+        text: "Válka! K čemu je dobrá? "
+    }, {
+        type: "black",
+        text: "Při sexu rád myslím na ___. "
+    },
+    {
+        type: "black",
+        text: "Co přede mnou rodiče skrývají? "
+    }, {
+        type: "black",
+        text: "Co ti spolehlivě dostane ženskou do postele? "
+    },
+    {
+        type: "black",
+        text: "V losangelském vězení se prý dá 200 cigaret vyměnit za ___. "
+    }, {
+        type: "black",
+        text: "Co jsem si přivezl z Turecka? "
+    },
+    {
+        type: "black",
+        text: "Co fakt nechceš najít v kuřecím kung pao? "
+    }, {
+        type: "black",
+        text: "Co si s sebou vezmu na cestu do minulosti, aby lidé uvěřili, že jsem mocný čaroděj? "
+    },
+    {
+        type: "black",
+        text: "Díky čemu si udržuju vztah? ___. "
+    }, {
+        type: "black",
+        text: "Broadway letos uvádí ___ v muzikálovém zpracování."
+    },
+    {
+        type: "black",
+        text: "Zatímco USA závodily se Sovětským svazem o přistání na Měsíci, vlévala mexická vláda miliony pesos do výzkumu ___. "
+    }, {
+        type: "black",
+        text: "Po zemětřesení na Haiti přinesl Sean Penn tamějším lidem ___. "
+    },
+    {
+        type: "black",
+        text: "Avšak než vás zabiji, pane Bonde, musím vám nejprve ukázat ___. "
+    }, {
+        type: "black",
+        text: "Z čeho dostávám nezadržitelné větry? "
+    },
+    {
+        type: "black",
+        text: "Jak páchnou staří lidi? "
+    }, {
+        type: "black",
+        text: "Třídní výlet naprosto zkazilo ___. "
+    },
+    {
+        type: "black",
+        text: "Když faraon zůstal netečný, přivolal Mojžíš smršť ___. "
+    },
+    {
+        type: "white",
+        text: "Hořící člověk. "
+    },
+    {
+        type: "white",
+        text: "Rasismus. "
+    },
+    {
+        type: "white",
+        text: "Stařecký pach. "
+    },
+    {
+        type: "white",
+        text: "Ženy z reklam na jogurty. "
+    },
+    {
+        type: "white",
+        text: "Třídně předpojaté podtóny. "
+    },
+    {
+        type: "white",
+        text: "Totální nezájem o Třetí svět. "
+    },
+    {
+        type: "white",
+        text: "Sexting"
+    },
+    {
+        type: "white",
+        text: "Větrný mlýn napěchovaný mrtvolami. "
+    },
+    {
+        type: "white",
+        text: "Přehnaně velké lízátko. "
+    },
+    {
+        type: "white",
+        text: "Děti v Africe. "
+    },
+    {
+        type: "white",
+        text: "Nesouměrná plastika prsou. "
+    },
+    {
+        type: "white",
+        text: "Přejídání a vyvrhování. "
+    },
+    {
+        type: "white",
+        text: "Tvrdě pracující Cikán. "
+    },
+    {
+        type: "white",
+        text: "Maličký koník. "
+    },
+    {
+        type: "white",
+        text: "Šušně"
+    },
+    {
+        type: "white",
+        text: "Barack Obama. "
+    },
+    {
+        type: "white",
+        text: "Sladké mámení. "
+    },
+    {
+        type: "white",
+        text: "Scientologie. "
+    },
+    {
+        type: "white",
+        text: "Dávivé křeče. "
+    },
+    {
+        type: "white",
+        text: "Bratrstvo kočičí pracky. "
+    },
+    {
+        type: "white",
+        text: "Darth Vader. "
+    },
+    {
+        type: "white",
+        text: "Fíkový koláč. "
+    },
+    {
+        type: "white",
+        text: "Fíkový koláč. "
+    },
+    {
+        type: "white",
+        text: "Rada od moudrého černého starce. "
+    },
+    {
+        type: "white",
+        text: "Postarší Japonec. "
+    },
+    {
+        type: "white",
+        text: "Ochutnávky zdarma. "
+    },
+    {
+        type: "white",
+        text: "Estrogen"
+    },
+    {
+        type: "white",
+        text: "Sexuální pnutí. "
+    },
+    {
+        type: "white",
+        text: "Hladomor. "
+    },
+    {
+        type: "white",
+        text: "Zatoulaný chlup. "
+    },
+    {
+        type: "white",
+        text: "Muži. "
+    },
+    {
+        type: "white",
+        text: "Pytlík kouzelných fazolí. "
+    },
+    {
+        type: "white",
+        text: "Represe. "
+    },
+    {
+        type: "white",
+        text: "Natřásání se. "
+    },
+    {
+        type: "white",
+        text: "Můj rodinný stav. "
+    },
+    {
+        type: "white",
+        text: "Přílišná kompenzace. "
+    },
+    {
+        type: "white",
+        text: "Navěky věků. "
+    },
+    {
+        type: "white",
+        text: "Lhostejnost vládnoucích elit k hrozbě globálního oteplování. "
+    },
+    {
+        type: "white",
+        text: "Torze varlat. "
+    },
+    {
+        type: "white",
+        text: "Sám Ďábel. "
+    },
+    {
+        type: "white",
+        text: "World of Warcraft. "
+    },
+    {
+        type: "white",
+        text: "Sexuální orientace Václava Klause. "
+    },
+    {
+        type: "white",
+        text: "Objektivizace ženského těla. "
+    },
+    {
+        type: "white",
+        text: "Obrázky prsou. "
+    },
+    {
+        type: "white",
+        text: "Něžné pohlazení po vnitřní straně stehna. "
+    },
+    {
+        type: "white",
+        text: "Amišové. "
+    },
+    {
+        type: "white",
+        text: "Budějovický Budvar. "
+    },
+    {
+        type: "white",
+        text: "Papež. "
+    },
+    {
+        type: "white",
+        text: "Létající sex-hadi. "
+    },
+    {
+        type: "white",
+        text: "Emma Watson. "
+    },
+    {
+        type: "white",
+        text: "Moje bývalá žena. "
+    },
+    {
+        type: "white",
+        text: "Sexy polštářové bitvy. "
+    },
+    {
+        type: "white",
+        text: "Další pitomý filmy o upírech."
+    },
+    {
+        type: "white",
+        text: "Kybernetické implantáty. "
+    },
+    {
+        type: "white",
+        text: "Civilní oběti. "
+    },
+    {
+        type: "white",
+        text: "Karel Gott. "
+    },
+    {
+        type: "white",
+        text: "Ženský orgasmus. "
+    },
+    {
+        type: "white",
+        text: "Skauti. "
+    },
+    {
+        type: "white",
+        text: "Osvětim"
+    },
+    {
+        type: "white",
+        text: "Malování prsty. "
+    },
+    {
+        type: "white",
+        text: "Michal David. "
+    },
+    {
+        type: "white",
+        text: "Židi. "
+    },
+    {
+        type: "white",
+        text: "Vytlačování na okraj společnosti. "
+    },
+    {
+        type: "white",
+        text: "Kristova krev. "
+    },
+    {
+        type: "white",
+        text: "Mrtví rodiče. "
+    },
+    {
+        type: "white",
+        text: "Um svádění. "
+    },
+    {
+        type: "white",
+        text: "Smrt na úplavici. "
+    },
+    {
+        type: "white",
+        text: "Mr. Proper, přímo za tebou. "
+    },
+    {
+        type: "white",
+        text: "Židovská bratrstva. "
+    },
+    {
+        type: "white",
+        text: "Hamé. "
+    },
+    {
+        type: "white",
+        text: "Falešný prorok. "
+    },
+    {
+        type: "white",
+        text: "Mirek Dušín. "
+    },
+    {
+        type: "white",
+        text: "Homosexuální agenda. "
+    },
+    {
+        type: "white",
+        text: "Miloš Zeman. "
+    },
+    {
+        type: "white",
+        text: "Pogrom. "
+    },
+    {
+        type: "white",
+        text: "Mimibazar. "
+    },
+    {
+        type: "white",
+        text: "Záře miliardy sluncí. "
+    },
+    {
+        type: "white",
+        text: "Lidé po amputaci. "
+    },
+    {
+        type: "white",
+        text: "Vržení panny do vulkánu. "
+    },
+    {
+        type: "white",
+        text: "Italové. "
+    },
+    {
+        type: "white",
+        text: "Výbuchy. "
+    },
+    {
+        type: "white",
+        text: "Pořádné šňupnutí. "
+    },
+    {
+        type: "white",
+        text: "Děti na vodítkách. "
+    },
+    {
+        type: "white",
+        text: "Katapulty. "
+    },
+    {
+        type: "white",
+        text: "Bilion dolarů. "
+    },
+    {
+        type: "white",
+        text: "Upevňování nadvlády. "
+    },
+    {
+        type: "white",
+        text: "Umírání. "
+    },
+    {
+        type: "white",
+        text: "Ticho. "
+    },
+    {
+        type: "white",
+        text: "Židozednářskoiluminátské spiknutí. "
+    },
+    {
+        type: "white",
+        text: "Justin Bieber. "
+    },
+    {
+        type: "white",
+        text: "Písmo svaté. "
+    },
+    {
+        type: "white",
+        text: "Koule. "
+    },
+    {
+        type: "white",
+        text: "Modlitba na zahnání teploušů. "
+    },
+    {
+        type: "white",
+        text: "BATMAN!"
+    }
+]
